e4ac502d55775571e7a5bc57a1e7e8a1c0d58e78
Tasneem Samy <tasneemsamy96@gmail.com>
2017-03-11 23:58:51 +0200

    sec commit

--- src/javaapplication27/JavaApplication27.java
+++ src/javaapplication27/JavaApplication27.java
@@ -16,8 +16,9 @@ public class JavaApplication27 {
      */
     public static void main(String[] args) {
         int x = 5;
-        int w = 3;
-        int e = x * w;
+        
+        int d = 3;
+        int e = x * d;
+        System.out.println(e);
     }
-    
 }
