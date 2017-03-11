c1eb1798557e5a4eb52f2631ecec93e4981d6bc3
Tasneem Samy <tasneemsamy96@gmail.com>
2017-03-11 23:24:28 +0200

    first commit

---
+++ src/javaapplication27/JavaApplication27.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication27;
+
+/**
+ *
+ * @author yassmin
+ */
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 5;
+        int w = 3;
+        int e = x * w;
+    }
+    
+}
